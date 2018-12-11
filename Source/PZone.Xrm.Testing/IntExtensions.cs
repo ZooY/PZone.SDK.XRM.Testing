de89c60fc2d0c1d97fdcf761be1f646ac8fbd5e3
Роман Копаев <roman@kopaev.ru>
2018-12-11 12:02:38 +0300

    no message

---
+++ Source/PZone.Xrm.Testing/IntExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PZone.Xrm.Testing
+{
+    public static class IntExtensions
+    {
+        public static Guid ToGuid(this int value)
+        {
+            return new Guid("00000000-0000-0000-0000"+value.ToString("000000000000"));
+        }
+    }
+}
